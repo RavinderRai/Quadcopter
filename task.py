e986b5f9de7044bcf592e8ff5e8981ff9bc74737
Ravi Rai <ravi.b.rai@outlook.com>
2021-10-25 21:02:00 -0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,100 @@
+import numpy as np
+from physics_sim import PhysicsSim
+import math as m
+       
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+       
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        reward = 1 + ((self.sim.pose[2]) - (self.target_pos[2]))
+        
+        if (self.sim.pose[2]) >= (self.target_pos[2]):
+            reward += (abs((self.sim.pose[2]) - (self.target_pos[2])))*15
+        
+        #if we are close to timing out, then penalize the reward
+        if abs(self.sim.runtime - self.sim.time) < 1:
+            if self.sim.done == False:
+                reward -= (abs(self.target_pos[2] - self.sim.pose[2]))
+            #reward for finishing early
+            else:
+                reward += abs(self.target_pos[2] - self.sim.pose[2])
+                
+        if (self.sim.pose[2] - self.sim.init_pose[2]) < 0.5:
+            reward -= (abs(self.target_pos[2] - self.sim.pose[2]))*10
+        
+        """
+        #this is some other stuff that I tried previously
+        if self.sim.done == True:
+            if self.target_pos[2] > self.sim.pose[2]:
+                reward -= abs(self.target_pos[2] - self.sim.pose[2])
+        
+        if (abs(self.sim.pose[:3] - self.target_pos)).sum() < 0.1:
+            
+            reward += ((abs(self.sim.pose[:3] - self.target_pos)).sum())
+            self.target_reached=True
+        else:
+            #penalize for timing out before
+            if self.sim.done == True:
+                reward -= ((abs(self.sim.pose[:3] - self.target_pos)).sum())
+            reward -= ((abs(self.sim.pose[:3] - self.target_pos)).sum())
+        
+ 
+        
+        #to prevent copter from straying too far in only one direction
+        if self.target_pos[1] > self.sim.pose[1] and self.target_pos[2] > self.sim.pose[2]:
+            if self.target_pos[0] < self.sim.pose[0]:
+                reward -= (abs(self.target_pos[0] - self.sim.pose[0]))
+        
+        if self.target_pos[1] > self.sim.pose[1] and self.target_pos[0] > self.sim.pose[0]:
+            if self.target_pos[2] < self.sim.pose[2]:
+                reward -= (abs(self.target_pos[2] - self.sim.pose[2]))  
+        
+        if self.target_pos[0] > self.sim.pose[0] and self.target_pos[2] > self.sim.pose[2]:
+            if self.target_pos[1] < self.sim.pose[1]:
+                reward -= (abs(self.target_pos[1] - self.sim.pose[1]))
+                
+        """
+        
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            #print(done)
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
